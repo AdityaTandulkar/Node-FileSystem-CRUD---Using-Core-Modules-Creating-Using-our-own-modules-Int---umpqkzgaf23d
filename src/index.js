ec32ce1fdf8a12829759a6a2b9d163e1faf87f8d
AdityaTandulkar <adityatandulkar20@gmail.com>
2023-04-23 20:39:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,24 +3,36 @@ const fs = require('fs/promises')
 const myFileWriter = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	fs.writeFile(fileName,fileContent,(err)=>{
+		if(err) console.log(err);
+	});
 }
 
 const myFileReader = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	fs.readFile(fileName, "utf8", (err, data)=>{
+		if(data) console.log(data);
+		else console.log(err);
+	})
 }
 
 
 const myFileUpdater = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	fs.appendFile(fileName, fileContent, (err)=>{
+		if(err) console.log(err);
+	})
 }
 
 const myFileDeleter = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	fs.unlink(fileName, (err)=>{
+		if(err) console.log(err);
+	})
 }
-
 
 
 module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter }
